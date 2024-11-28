2efda669e96aa1ecb1fa75ad19d47a46157d12e0
Canthar90 <d.cieslak90@gmail.com>
2024-11-28 16:46:26 +0100

    Submited data to form

--- main.py
+++ main.py
@@ -24,13 +24,36 @@ driver.get("https://demoqa.com/login")
 
 username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "userName")))
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "password")))
-login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login")))
+# login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login")))
+login_button = driver.find_element(By.ID, "login")
 
 
 username_field.send_keys(username)
 password_field.send_keys(passw)
-login_button.click()
+driver.execute_script("arguments[0].click();", login_button)
 
+# Locate the Element dropdown and Textbox 
+elements = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div')))
+
+elements.click()
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="item-0"]')))
+text_box.click()
+
+# Locate the form fields and submit button
+full_name_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+email_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail')))
+current_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress')))
+permanent_address_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentAddress')))
+submit_button = driver.find_element(By.ID, 'submit')
+
+
+# Fill in the from Fields
+full_name_field.send_keys("John Smith")
+email_field.send_keys("johnsmith@example.com")
+current_address_field.send_keys("John Street 100 New York USA")
+permanent_address_field.send_keys("John Street 100 New York USA")
+driver.execute_script("arguments[0].click();", submit_button)
 
 input("Press enter to close the browser")
 driver.quit()
