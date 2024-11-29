9c1708af43a4638fed019f161bfca3db6325fe6d
Canthar90 <d.cieslak90@gmail.com>
2024-11-29 16:47:06 +0100

    Gui connected to actions from backend

--- gui.py
+++ gui.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import messagebox
+from main import WebAutomation
+
 
 class App:
     def __init__(self, root):
@@ -11,26 +14,32 @@ class App:
         self.login_frame.pack(padx=10, pady=10)
 
         tk.Label(self.login_frame, text="Username").grid(row=0, column=0, sticky="w")
-        self.entry_username = tk.Entry(self.login_frame).grid(row=0, column=1, sticky="ew")
+        self.entry_username = tk.Entry(self.login_frame)
+        self.entry_username.grid(row=0, column=1, sticky="ew")
 
         tk.Label(self.login_frame, text="Password").grid(row=1, column=0, sticky="w")
-        self.entry_password = tk.Entry(self.login_frame).grid(row=1, column=1, sticky="ew")
+        self.entry_password = tk.Entry(self.login_frame, show="*")
+        self.entry_password.grid(row=1, column=1, sticky="ew")
 
         # Form submission frame
         self.form_frame = tk.Frame(self.root)
         self.form_frame.pack(padx=10, pady=10)
 
         tk.Label(self.form_frame, text="Full Name").grid(row=0, column=0, sticky="w")
-        self.entry_fullname = tk.Entry(self.form_frame).grid(row=0, column=1, sticky="ew")
+        self.entry_fullname = tk.Entry(self.form_frame)
+        self.entry_fullname.grid(row=0, column=1, sticky="ew")
 
         tk.Label(self.form_frame, text="Email").grid(row=1, column=0, sticky="w")
-        self.entry_email = tk.Entry(self.form_frame).grid(row=1, column=1, sticky="ew")
+        self.entry_email = tk.Entry(self.form_frame)
+        self.entry_email.grid(row=1, column=1, sticky="ew")
 
         tk.Label(self.form_frame, text="Current Address").grid(row=2, column=0, sticky="w")
-        self.entry_current_address = tk.Entry(self.form_frame).grid(row=2, column=1, sticky="ew")
+        self.entry_current_address = tk.Entry(self.form_frame)
+        self.entry_current_address.grid(row=2, column=1, sticky="ew")
 
         tk.Label(self.form_frame, text="Permanent Address").grid(row=3, column=0, sticky="w")
-        self.entry_permanent_address = tk.Entry(self.form_frame).grid(row=3, column=1, sticky="ew")
+        self.entry_permanent_address = tk.Entry(self.form_frame)
+        self.entry_permanent_address.grid(row=3, column=1, sticky="ew")
 
 
         # Buttons
@@ -43,10 +52,24 @@ class App:
 
 
     def submit_data(self):
-        pass
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+        fullname = self.entry_fullname.get()
+        email = self.entry_email.get()
+        current_address = self.entry_current_address.get()
+        permanent_address = self.entry_permanent_address.get()
+
+        self.web_automation = WebAutomation()
+        self.web_automation.login(username, password)
+        self.web_automation.fill_form(fullname, email, current_address, permanent_address)
+        self.web_automation.download()
+        
+        messagebox.showinfo("Data Submited Succesfully")
+
 
     def close_browser(self):
-        pass
+        self.web_automation.close()
+        
 
 
 
